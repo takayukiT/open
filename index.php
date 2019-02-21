277967bd48fa3c0bc914fff1cb5f4686558681f8
tsuchida <tsuchida@toba-cmt.ac.jp>
2019-02-21 10:06:51 +0900

    make it better

--- index.php
+++ index.php
@@ -1,6 +1,6 @@
 <?php
 echo "Hello5";
-require_once __DIR__ . 'vendor\autoload.php';
+require_once __DIR__ . '\vendor\autoload.php';
 
 $inputString = file_get_contents('php://input');
 error_log($inputString);
