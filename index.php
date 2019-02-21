f2ebe8f447fbef9218fd31456fd1ed2fb45f2bff
tsuchida <tsuchida@toba-cmt.ac.jp>
2019-02-21 13:01:47 +0900

    php change commit

--- index.php
+++ index.php
@@ -7,7 +7,7 @@ error_log($inputString);
 
 $httpClient = new \LINE\LINEbot\HTTPClient\CurLHTTPClient(getenv('CHANNEL_ACCCESS_TOKEN'));
 $bot = new \LINE\LINEbot($httpClient,['channelSecret'=>getenv('channel_SECRET')])
-$signature = $_SERVER['HTTP_' . \LINE\LINEbot\Constant\HTTPHeader::LINE_SIGNATURE]
+$signature = $_SERVER['HTTP_' . \LINE\LINEbot\Constant\HTTPHeader::LINE_SIGNATURE];
 
 $events = $bot->parseEventRequest(file_get_contents('php://input'),$signature);
 foreach ($events as $event) {
