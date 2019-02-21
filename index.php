5ecd09218e5072ca44ba8214936a4824179bb8f8
tsuchida <tsuchida@toba-cmt.ac.jp>
2019-02-21 13:42:40 +0900

    php change commit

--- index.php
+++ index.php
@@ -1,10 +1,10 @@
 <?php
-
+require_once __DIR__ . ("/vendor/autoload.php");
 use \LINE\LINEbot\Constant\HTTPHeader;
 use \LINE\LINEbot\HTTPClient\CurLHTTPClient;
 
 echo "Hello2";
-require_once __DIR__ . ("/vendor/autoload.php");
+
 
 $inputString = file_get_contents('php://input');
 error_log($inputString);
