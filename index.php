956a87e774bb323b809465d58678af7a0aa7883c
tsuchida <tsuchida@toba-cmt.ac.jp>
2019-02-21 11:39:08 +0900

    php change commit

--- index.php
+++ index.php
@@ -1,6 +1,6 @@
 <?php
 echo "Hello2";
-require_once __DIR__ . ("/autoload.php");
+require_once __DIR__ . ("/vendor/autoload.php");
 
 $inputString = file_get_contents('php://input');
 error_log($inputString);
