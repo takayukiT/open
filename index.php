9b8f42b9431d7e407835008b0f91d548963bac55
tsuchida <tsuchida@toba-cmt.ac.jp>
2019-02-21 10:09:45 +0900

    make it better

--- index.php
+++ index.php
@@ -1,6 +1,6 @@
 <?php
-echo "Hello4";
-require_once __DIR__ . '\vendor\autoload.php';
+echo "Hello3";
+//require_once __DIR__ . '\vendor\autoload.php';
 
 $inputString = file_get_contents('php://input');
 error_log($inputString);
