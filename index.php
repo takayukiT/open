9d28aee013ca15dbd48652e6da999a178452f30f
tsuchida <tsuchida@toba-cmt.ac.jp>
2019-02-21 10:02:14 +0900

    make it better

--- index.php
+++ index.php
@@ -1,6 +1,6 @@
 <?php
-echo "Hello3";
-require_once __DIR__ . 'vender/autoload.php';
+echo "Hello4";
+require_once __DIR__ . 'C:\Users\tsuchida\heroku_project\vendor\autoload.php';
 
 $inputString = file_get_contents('php://input');
 error_log($inputString);
