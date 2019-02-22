985a971eb21c549a7a8cb48e5c0d49df13e6e942
tsuchida <tsuchida@toba-cmt.ac.jp>
2019-02-23 00:10:03 +0900

    php change commit

--- index.php
+++ index.php
@@ -34,12 +34,40 @@ foreach ($events as $event) {
 //$bot->replyText($event->getReplyToken(),'return-tsuchidaB');
 //  $messageBuilder = new MultiMessageBuilder();
 //  $messageBuilder->add(new ImageMessageBuilder(
-$messageBuilder=new ImageMessageBuilder(
+/*$messageBuilder=new ImageMessageBuilder(
     "https://kosenjp-my.sharepoint.com/:i:/g/personal/tsuchida_toba_kosen-ac_jp/EWqdgG77ZCtLqLwldvNPeM4Bb68DYlax5jq-6UYPfSop4A?e=HaTfBb",
     "https://kosenjp-my.sharepoint.com/:i:/g/personal/tsuchida_toba_kosen-ac_jp/EWqdgG77ZCtLqLwldvNPeM4Bb68DYlax5jq-6UYPfSop4A?e=HaTfBb"
 );
   //$bot->replyMessage($event->replyToken, $messageBuilder);
-  $bot->replyMessage($event->getReplyToken(), $messageBuilder);
+  $bot->replyMessage($event->getReplyToken(), $messageBuilder);*/
+
+  $receive = json_decode($inputString, true);
+
+  $event = $receive['events'][0];
+  $reply_token  = $event['replyToken'];
+
+  $headers = array('Content-Type: application/json',
+                   'Authorization: Bearer ' . $accessToken);
+
+//  $message = 【ここが送信するオブジェクトにより異なります】
+
+  $message = array('type'               => 'image',
+                   'originalContentUrl' => 'https://kosenjp-my.sharepoint.com/:i:/g/personal/tsuchida_toba_kosen-ac_jp/EWqdgG77ZCtLqLwldvNPeM4Bb68DYlax5jq-6UYPfSop4A?e=HaTfBb',
+                   'previewImageUrl'    => 'https://kosenjp-my.sharepoint.com/:i:/g/personal/tsuchida_toba_kosen-ac_jp/EWqdgG77ZCtLqLwldvNPeM4Bb68DYlax5jq-6UYPfSop4A?e=HaTfBb');
+
+  $body = json_encode(array('replyToken' => $reply_token,
+                            'messages'   => array($message)));
+  $options = array(CURLOPT_URL            => 'https://api.line.me/v2/bot/message/reply',
+                   CURLOPT_CUSTOMREQUEST  => 'POST',
+                   CURLOPT_RETURNTRANSFER => true,
+                   CURLOPT_HTTPHEADER     => $headers,
+                   CURLOPT_POSTFIELDS     => $body);
+
+  $curl = curl_init();
+  curl_setopt_array($curl, $options);
+  curl_exec($curl);
+  curl_close($curl);
+
   }
 
 
